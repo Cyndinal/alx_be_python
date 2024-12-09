bb431eed8b103303bc0dee09bcde5e0a4dbfb1a3
Max Cydin <nyarkomaxwell03@gmail.com>
2024-12-08 18:23:51 -0700

    Fixing

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,4 +1,6 @@
 
+
+
 def addition(number1=10,number2=5):
     summer = number1 + number2
     print(f"Sum of {number1} and {number2} is {summer}")
@@ -13,6 +15,9 @@ def multiplication(number1=10,number2=5):
     mul = number1 * number2
     print(f"Multiplication of {number1} and {number2} is {mul}")
 
+addition()
+subtraction()
+multiplication()
 
 
 
