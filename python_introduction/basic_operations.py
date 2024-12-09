b5e6a94cbef1a6b3b4659d5d4e8ba79b610b7f71
Max Cydin <nyarkomaxwell03@gmail.com>
2024-12-08 18:13:08 -0700

    Operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,21 @@
+
+def addition(number1,number2):
+    summer = number1 + number2
+    print(f"Sum of {number1} and {number2} is {summer}")
+
+
+def subtraction(number1,number2):
+    difference = number1 - number2
+    print(f"Subtraction of {number1} and {number2} is {difference}")
+
+
+def multiplication(number1,number2):
+    mul = number1 * number2
+    print(f"Multiplication of {number1} and {number2} is {mul}")
+
+
+
+
+addition(5,10)
+multiplication(5,10)
+subtraction(5,10)
