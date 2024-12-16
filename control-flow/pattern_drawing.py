8a98aba03245e948ed2aa2e63c862a7e331384ed
Max Cydin <nyarkomaxwell03@gmail.com>
2024-12-16 01:09:06 -0700

    coding

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,7 +2,8 @@ user = int(input("Enter the size of the pattern:"))
 
 while user > 0:
     for i in range(1,user+1):
-        print("*" * user,end="\n")
+        print("*" * user,end=""+"\n")
+
     break
 
 
