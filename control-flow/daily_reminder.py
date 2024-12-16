1c595f6b920c8c83d0930cd83df5201f439df89e
Max Cydin <nyarkomaxwell03@gmail.com>
2024-12-16 00:58:19 -0700

    coding

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,3 @@
+task = input("Task description: ")
+priority = input("Task priority: medium/high/low ")
+time_bound =input("Task time bound: yes/no ")
---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+user = int(input("Enter the size of the pattern:"))
+
+while user > 0:
+    for i in range(1,user+1):
+        print("*" * user,end="\n")
+    break
+
+
+
