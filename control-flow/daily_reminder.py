5c3aff922eef7bcc93f8dd00bcc1a4e018a17594
Max Cydin <nyarkomaxwell03@gmail.com>
2024-12-16 10:55:12 -0700

    coding

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,3 +1,20 @@
+from babel.messages.checkers import checkers
+
 task = input("Task description: ")
-priority = input("Task priority: medium/high/low ")
-time_bound =input("Task time bound: yes/no ")
+priority = input("Task priority: low/medium/high ").lower()
+time_bound = input("Task time bound: yes/no ").lower()
+
+task_template = {
+    "task": task,
+    "priority":priority,
+    "time_bound":time_bound
+}
+
+match priority:
+    case "high":
+        if priority == "high" and time_bound == "yes":
+            print(f"Reminder: {task} is a high priority task that requires immediate attention today!")
+
+    case "low":
+        if priority == "low" and time_bound == "yes":
+            print(f"{task_template.get(task)} is a low priority task. Consider completing it when you have free time")
