285cf416d79e9eecaff6610822a57c0d9b57d4fc
Max Cydin <nyarkomaxwell03@gmail.com>
2025-01-10 15:52:36 -0700

    Arithmetic functions

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,28 +1,45 @@
-from numpy.f2py.auxfuncs import throw_error
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Error: Invalid operation"
 
 
-def perform_operation(num1,num2,operation):
-    # num1 =float(input("Numb1"))
-    # num2 =float(input("Numb2"))
-    # operation = input("Enter the operation to be performed: add/subtract/multiply/divide").lower()
-    match operation:
-        case "add":
-            result = num1+num2
-            return result
-        case "subtract":
-            result = num1-num2
-            return result
-        case "multiply":
-            result = num1*num2
-            return result
-        case "divide":
-            try:
-                result = num1/num2
-                return result
-            except ZeroDivisionError:
-                if num2 == 0:
-                    throw_error(f"{num2} cannot be zero, Review!")
-        case _:
-            print("Invalid operation")
+print("The arithmetic_operations.py script has been created successfully.")
+
+
+
+# def perform_operation(num1,num2,operation):
+#     # num1 =float(input("Numb1"))
+#     # num2 =float(input("Numb2"))
+#     # operation = input("Enter the operation to be performed: add/subtract/multiply/divide").lower()
+#     match operation:
+#         case "add":
+#             result = num1+num2
+#             return result
+#         case "subtract":
+#             result = num1-num2
+#             return result
+#         case "multiply":
+#             result = num1*num2
+#             return result
+#         case "divide":
+#             try:
+#                 result = num1/num2
+#                 return result
+#             except ZeroDivisionError:
+#                 return f"{num1} cannot be divided by zero"
+#
+#         case _:
+#             print("Invalid operation")
 
 
