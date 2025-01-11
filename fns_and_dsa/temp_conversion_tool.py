c719166845254501d9604a695fa600d35dbe5ee3
Max Cydin <nyarkomaxwell03@gmail.com>
2025-01-11 14:23:05 -0700

    temperature conversion

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,23 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+def convert_to_celsius(fahrenheit):
+    print(f"{(fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR}°C")
+
+
+def convert_to_fahrenheit(celsius):
+    print(f"{(celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32}°F")
+
+
+
+prompt = int(input("Enter the temperature in Fahrenheit or Celsius: "))
+celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/F):").strip().upper()
+
+match celsius_or_fahrenheit:
+    case "C":
+        convert_to_fahrenheit(prompt)
+    case "F":
+        convert_to_celsius(prompt)
+    case _:
+        print("Invalid input, Enter C or F ")
+
