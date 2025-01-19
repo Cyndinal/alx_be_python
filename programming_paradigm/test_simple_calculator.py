96eda24fea35d401b43217685d2ea9a3fc408a76
Max Cydin <nyarkomaxwell03@gmail.com>
2025-01-19 06:04:52 -0700

    Tester

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,14 +10,12 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def test_addition(self):
         self.assertEqual(self.calc.add(2,1), 3)
-"""
-        ["test_addition", " self.assertEqual(self.calc.add", "test_addition(self)"]
-"""
-    def test_subtract(self):
+
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(2,1), 1)
     def multiplication(self):
         self.assertEqual(self.calc.multiply(2,2), 4)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(2,2), 1)
 
 
