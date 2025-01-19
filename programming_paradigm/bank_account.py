0499292ae64613996e99c00b76fb1481dd477681
Max Cydin <nyarkomaxwell03@gmail.com>
2025-01-19 05:17:32 -0700

    balance bank

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,10 +6,11 @@ class BankAccount:
     def deposit(self,amount):
         self.account_balance +=amount
     def withdraw(self,amount):
-        self.account_balance -= amount
-        print(f"Withdrew {amount}")
         if amount > self.account_balance:
             print("Insufficient balance")
+        else:
+            self.account_balance -= amount
+            print(f"Withdrew ${amount}")
     def display_balance(self):
         self.account_balance = float(self.account_balance)
         print(f"Current Balance: ${self.account_balance}")
